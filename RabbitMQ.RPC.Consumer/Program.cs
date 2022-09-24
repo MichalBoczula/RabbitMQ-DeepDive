a1ffe3d300e2a75d322822d440546ae99b212b0f
Michal Boczula <michal.boczula0101@gmail.com>
2022-09-24 23:47:05 +0200

    RPC

--- RabbitMQ.RPC.Consumer/Program.cs
+++ RabbitMQ.RPC.Consumer/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
 
 namespace RabbitMQ.RPC.Consumer
 {
@@ -6,7 +13,29 @@ namespace RabbitMQ.RPC.Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var replyTo = e.BasicProperties.ReplyTo;
+                var correlationId = e.BasicProperties.CorrelationId;
+
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Message: {message} {replyTo} {correlationId}");
+
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: "MessageRPC",
+                                 autoAck: false, 
+                                 consumer: consumer);
+
+            Console.ReadLine();
         }
     }
 }
--- RabbitMQ.RPC.Producer/Program.cs
+++ RabbitMQ.RPC.Producer/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
 namespace RabbitMQ.RPC.Producer
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.QueueDeclare(queue: "MessageRPC",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            channel.QueueDeclare(queue: "AcknowledgeRRPC",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var props = channel.CreateBasicProperties();
+            props.Persistent = true;
+
+            props.CorrelationId = Guid.NewGuid().ToString();
+            props.ReplyTo = "AcknowledgeRRPC";
+
+            string message = "rpc";
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            while (true)
+            {
+                channel.BasicPublish(exchange: "",
+                             routingKey: "MessageRPC",
+                             basicProperties: props,
+                             body: body);
+
+                //channel.BasicConsume(
+                //            consumer: consumer,
+                //            queue: props.ReplyTo,
+                //            autoAck: true);
+
+                Thread.Sleep(2000);
+            }
         }
     }
 }
